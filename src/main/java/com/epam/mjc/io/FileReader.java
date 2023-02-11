8f3b276e512c129784a406743a59da3c8b57f2a3
Nastya Garkavaya <anastaishastil2002@gmail.com>
2023-02-11 12:47:15 +0300

    Commit 3

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -2,6 +2,8 @@ package com.epam.mjc.io;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class FileReader {
@@ -15,19 +17,20 @@ public class FileReader {
                 str = str.concat(Character.toString(c));
             }
             String[] lines = str.split("\n");
-            profile.setName(lines[0].substring(lines[0].indexOf(':') + 2, lines[0].length()-1));
-            String age = lines[1].substring(lines[1].indexOf(':') + 2, lines[1].length()-1);
+            profile.setName(lines[0].substring(lines[0].indexOf(':') + 2));
+            String age = lines[1].substring(lines[1].indexOf(':') + 2);
             if (!age.equals("")) {
                 profile.setAge(Integer.parseInt(age));
             }
-            profile.setEmail(lines[2].substring(lines[2].indexOf(':') + 2, lines[2].length()-1));
-            String phone = lines[1].substring(lines[1].indexOf(':') + 2, lines[1].length()-1);
+            profile.setEmail(lines[2].substring(lines[2].indexOf(':') + 2));
+            String phone = lines[3].substring(lines[3].indexOf(':') + 2);
             if (!phone.equals("")) {
                 profile.setPhone(Long.parseLong(phone));
             }
         } catch (IOException ex) {
-
-            System.err.println(ex);
+            Logger logger = Logger.getLogger(
+                    FileReader.class.getName());
+            logger.log(Level.WARNING, ex.toString());
         }
         return profile;
     }
